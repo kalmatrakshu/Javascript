df4bd38e7164dc53aff3b32eb29c28c7ea22a5fb
kalmatrakshu <92665074+kalmatrakshu@users.noreply.github.com>
2022-03-03 23:56:44 +0530

    Add files via upload

---
+++ MirrorPattern.java
@@ -0,0 +1,32 @@
+package arrayExamples;
+
+import java.util.Scanner;
+
+public class MirrorPattern {
+	 private static Scanner sc;
+		
+		public static void main(String[] args) {
+			sc = new Scanner(System.in);
+			
+			System.out.print("Enter Mirrored Right Triangle Pattern Rows = ");
+			int rows = sc.nextInt();
+			
+			System.out.println("Printing Mirrored Right Triangle Star Pattern");
+			
+			for (int i = 1 ; i <= rows; i++ ) 
+			{
+				for (int j = 0 ; j < rows - i; j++ ) 
+				{
+					System.out.print(" ");
+				}
+				for (int k = 0 ; k < i; k++ ) 
+				{
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+		}      
+          
+		
+
+}
