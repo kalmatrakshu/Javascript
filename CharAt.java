9a22237befa194791894d942f6070aab1c5b2292
kalmatrakshu <92665074+kalmatrakshu@users.noreply.github.com>
2022-03-04 00:53:27 +0530

    Add files via upload

---
+++ CharAt.java
@@ -0,0 +1,15 @@
+package com.capgemini.tranning;
+
+public class CharAt {
+
+	public static void main(String[] args) {
+		String obj="i love capgemini";
+		System.out.println(obj.charAt(3));
+		System.out.println(obj.toUpperCase());
+		
+		String obj2="HELLO CAPGEMINI";
+		System.out.println(obj2.toLowerCase());
+
+	}
+
+}
