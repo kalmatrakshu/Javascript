16f5e22ffaab0b51dd1261676ae34a9a8822a58c
kalmatrakshu <92665074+kalmatrakshu@users.noreply.github.com>
2022-03-03 21:16:07 +0530

    Add files via upload

---
+++ Employeedetails.java
@@ -0,0 +1,20 @@
+package classandObject;
+
+public class Employeedetails {
+
+	public static void main(String[] args) {
+		        Employee emp = new Employee();
+				emp.readEmployeeDetails1();
+				emp.displayEmployeeDetails1();
+				
+				Employee emp1 = new Employee();
+				emp1.readEmployeeDetails1();
+				emp1.displayEmployeeDetails1();
+			}
+
+		
+
+
+	}
+
+
