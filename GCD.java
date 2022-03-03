0908ad2e4dbd031910ad5361f0ea964b9bb71084
kalmatrakshu <92665074+kalmatrakshu@users.noreply.github.com>
2022-03-03 23:05:17 +0530

    Add files via upload

---
+++ GCD.java
@@ -0,0 +1,35 @@
+package stringsExample;
+	import java.util.Scanner;
+
+	public class GCD {
+
+		public static void main(String[] args) {
+			
+			int a,b,gcd=0;
+			Scanner sc=new Scanner(System.in);
+					System.out.println("enter the first number");
+			a=sc.nextInt();
+			System.out.println("enter the second number");
+			b=sc.nextInt();
+			gcd=findGCD(a,b);
+			System.out.println("GCD of" + a + "and" +b+ "="+gcd);
+		}
+		
+		public static int findGCD(int a, int b)
+		{
+			while(b!=0);
+			{
+				if(a>b) {
+					a=a-b;
+				}
+				else {
+					b=b-a;
+				}
+			}
+			
+	return a;
+		}
+
+	}
+
+
